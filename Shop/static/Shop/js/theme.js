16bbab2cd6d93d031a44e9ea3596e876d218156e
Andreika02 <andreyzur0207@gmail.com>
2024-08-25 15:33:16 +0300

    Добавили модель юзера

--- Shop/static/Shop/js/theme.js
+++ Shop/static/Shop/js/theme.js
@@ -3,16 +3,33 @@ const body = document.querySelector("body")
 
 btnChangeTheme.addEventListener("click", toggleTheme)
 let currentTheme = true
-
+loadTheme()
 
 function setTheme(value) {
     if (value === true) {
         btnChangeTheme.src = '/static/Shop/img/sun.png'
-        body.setAttribute("data-bs-theme", "light") 
+        document.body.setAttribute("data-bs-theme", "light") 
+        localStorage.setItem("theme", value)
     }
     else{
         btnChangeTheme.src = '/static/Shop/img/moon.png'
-        body.setAttribute("data-bs-theme", "dark") 
+        document.body.setAttribute("data-bs-theme", "dark") 
+        localStorage.setItem("theme", value)
+    }
+    
+}
+
+function loadTheme(){
+    const loaded_theme = localStorage.getItem("theme")
+    if (loaded_theme !== null){
+        if (loaded_theme === "true"){
+            setTheme(true)
+            currentTheme = true
+        }
+        else{
+            setTheme(false)
+            currentTheme = false
+        }
     }
 }
 
