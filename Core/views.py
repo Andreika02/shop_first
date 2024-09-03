112cd3769853a70d3579a0e5d0697981da284c53
Andreika02 <andreyzur0207@gmail.com>
2024-09-03 18:44:19 +0300

    Доделали сайт

--- Core/views.py
+++ Core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from Core.models import User
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 
